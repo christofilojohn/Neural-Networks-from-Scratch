53e9563b0c4c6739ceeb02cda5e338c531903cd3
Ioannis Christofilogiannis <ichristofilogian@tuc.gr>
2023-06-30 16:28:29 +0300

    initialization

---
+++ neural_network_testing.py
@@ -0,0 +1,8 @@
+# Building a neural Network from scratch
+# This is a test to fit a sine wave with an array of neurons without an optimizer
+
+import numpy as np
+
+inputs = []
+class Layers_Dense:
+    pass
