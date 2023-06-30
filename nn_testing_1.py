e797cec7a1ceb4922bad52cb55772237eca0da66
Ioannis Christofilogiannis <ichristofilogian@tuc.gr>
2023-06-30 17:06:33 +0300

    commit 1st test

---
+++ nn_testing_1.py
@@ -0,0 +1,29 @@
+# Building a neural Network from scratch
+# This is a test to fit a sine wave with an array of neurons without an optimizer
+
+import numpy as np
+np.random.seed(0)
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+
+output = []
+tableX = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+
+
+print(output)
+
+class Layers_Dense:
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons ))
+    def forward(self,inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layers_Dense(4,5)
+layer2 = Layers_Dense(5,2)
+
+layer1.forward(tableX)
+layer2.forward(layer1.output)
+print(layer2.output)
