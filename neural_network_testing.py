b8b416f372f8ed7cd3fa31170ac46f5a5536d312
Ioannis Christofilogiannis <ichristofilogian@tuc.gr>
2023-06-30 16:39:39 +0300

    testing with ReLU activation

--- neural_network_testing.py
+++ neural_network_testing.py
@@ -3,6 +3,21 @@
 
 import numpy as np
 
-inputs = []
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+# ReLU activation function testing
+# method 1:
+for i in inputs:
+    if (i>0):
+        output.append(i)
+    else:
+        output.append(0)
+# method 2:
+for i in inputs:
+    output.append(max(0,i))
+
+
+print(output)
 class Layers_Dense:
     pass
